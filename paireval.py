6ea736753721d96a96dfbcd49a200785af73cccf
Artem Sokolov <artem.sokolov@gmail.com>
2023-03-31 01:43:55 +0000

    Added naive O(n^2) implementation

--- paireval.py
+++ paireval.py
@@ -33,7 +33,7 @@ def paired_eval(scores, labels, min_dist=0.5):
 
     # Start by separating the two pointers by at least min_dist
     while (sl[r] - sl[l]) < min_dist and r < len(sl) - 1:
-        r = r + 1
+        r += 1
     
     # Reaching the end of the array indicates there are no rankable pairs
     if (sl[r] - sl[l]) < min_dist:
@@ -43,12 +43,12 @@ def paired_eval(scores, labels, min_dist=0.5):
     
         # Catch the left pointer up, keeping it at least min_dist away
         while (sl[r] - sl[l+1]) >= min_dist:     # +1 to look ahead
-            l = l + 1
+            l += 1
         
         # Everything from the beginning of the array up to (and including) l is
         #   a rankable pair relative to r
         nrp = nrp + (l + 1)      # +1 to account for 0-based indexing
-        r = r + 1
+        r += 1
     
     # Compute the number of misranked pairs through inversion counting
     arr = np.array(labels)[np.argsort(scores)]      # Array for merge sort
@@ -89,3 +89,38 @@ def paired_eval(scores, labels, min_dist=0.5):
 
     # Ranked correctly = total - misranked
     return nrp, nrp - _merge_sort(0, len(arr)-1)
+
+def naive_eval(scores, labels, min_dist=0.5):
+    '''
+    The naive O(n^2) implementation of paired evaluation
+    https://doi.org/10.1101/2022.09.07.507020
+    
+    Used for testing.
+    
+    Parameters and return values are as in paired_eval()
+    '''
+    
+    # Argument verification
+    if len(scores) < 2:
+        raise Exception("Paired eval requires at least one rankable pair")
+    if len(scores) != len(labels):
+        raise Exception("The number of scores and labels must be the same")
+
+    
+    n = 0   # Total rankable
+    m = 0   # Ranked correctly
+    
+    for j in range(1, len(scores)):
+        for i in range(i):
+            
+            # Is (i, j) rankable?
+            if np.abs(labels[i] - labels[j]) >= min_dist:
+                n += 1
+            else:
+                continue
+                
+            # Is (i, j) ranked correctly?
+            if np.sign(scores[i] - scores[j]) == np.sign(labels[i] - labels[j]):
+                m += 1
+                
+    return n, m
